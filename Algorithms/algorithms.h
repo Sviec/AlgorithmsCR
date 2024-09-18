36774e2c87258adaba34ecfb73a0b0e8054229e2
Sviec <Sviec-I@yandex.ru>
2024-09-19 00:01:18 +0300

    Implemented the task of finding the shortest path

---
+++ Algorithms/algorithms.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+void the_most_effective_way_dp();
+
+
+#endif ALGORITHMS_H
---
+++ Algorithms/main.cpp
@@ -0,0 +1,9 @@
+﻿#include "algorithms.h"
+#include <iostream>
+
+int main()
+{
+    the_most_effective_way_dp();
+
+    return 0;
+}
